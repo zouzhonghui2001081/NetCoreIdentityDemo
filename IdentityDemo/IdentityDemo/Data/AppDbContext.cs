dfd91281e04ffa8a4bfe15dfab4d8d2a0f2a3091
zouzhonghui2001081 <zhonghui.zou@perkinelmer.com>
2022-05-01 21:31:55 +0800

    First version add the Identity project

---
+++ IdentityDemo/IdentityDemo/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityDemo.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ IdentityDemo/IdentityDemo/Program.cs
@@ -0,0 +1,35 @@
+using IdentityDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", false)
+    .Build();
+
+var conString = config.GetConnectionString("default");
+builder.Services.AddDbContext<AppDbContext>(o => o.UseNpgsql(conString));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
